60e713e7425121ef8a770e7b0d1f4ed9f7a4e8d7
itsOasi <ostally001@gmail.com>
2023-10-20 15:49:07 -0700

    created modal

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,9 +1,11 @@
+import Button from "./Button"
+
 function Filter() {
 	let filters = ["Art", "Music", "3D"] 
   
 	return (
 		<div id='filter' className="col">
-			{filters.map(filter => <p key={filter}>{filter}</p>)}
+			{filters.map(filter => <p key={filter} className="filter_item">{filter}</p>)}
 		</div>
 	)
   }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import * as Icons from 'react-bootstrap-icons'
+import Button from './Button'
 
 function Header() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-		<div className='header'>
+		<div id='header'>
 			<img className='logo' src="./logo_full_22_12_6.png"></img>
 			<div className='nav row'>
-				<a href='https://instagram.com/roseling_gd?igshid=OGQ5ZDc2ODk2ZA==' target='_blank'>
-					<Icons.Instagram className='icon' />
-				</a>
+				<Button href='https://instagram.com/roseling_gd?igshid=OGQ5ZDc2ODk2ZA==' msg={<Icons.Instagram className='icon' />}></Button>
+				<Button action="true" href="https://donate.stripe.com/cN24gP5PM1Z371mdQQ" msg="Support Us"></Button>
 			</div>
 		</div>
     </>
